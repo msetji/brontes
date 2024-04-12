0e53dbe9bd64edfe4f71197b689941d1ae5c9554
demattosanthony <demattosanthony@gmail.com>
2024-04-12 15:19:45 -0400

    git action docker ip address

--- tests/infrastructure/test_knowledge_graph.py
+++ tests/infrastructure/test_knowledge_graph.py
@@ -8,7 +8,7 @@ class TestKnowledgeGraph(unittest.TestCase):
   def setUp(self):
     username = "neo4j"
     password = "neo4j-password"
-    url = "bolt://localhost:7687"
+    url = "bolt://172.17.0.1:7687"
     neo4j_container = (
         DockerContainer(image="neo4j_with_plugins")
         .with_exposed_ports(7687)
@@ -17,6 +17,7 @@ class TestKnowledgeGraph(unittest.TestCase):
     neo4j_container.start()
     time.sleep(30)
     # wait_for_logs(neo4j_container, "Started", timeout=30)
+    print("Neo4J Logs:", neo4j_container.get_logs())
     self.neo4j_container = neo4j_container
 
     self.kg = KnowledgeGraph(neo4j_uri=url, neo4j_user=username, neo4j_password=password)
