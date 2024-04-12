79f57f395452748cb6fb0b60d3e823ac1b96185f
demattosanthony <demattosanthony@gmail.com>
2024-04-12 15:34:56 -0400

    test knowledge graph working locally, updated dockerfile to try to work on git

--- tests/infrastructure/test_knowledge_graph.py
+++ tests/infrastructure/test_knowledge_graph.py
@@ -6,19 +6,22 @@ import time
 
 class TestKnowledgeGraph(unittest.TestCase):
   def setUp(self):
-    username = "neo4j"
-    password = "neo4j-password"
-    url = "bolt://172.17.0.1:7687"
     neo4j_container = (
         DockerContainer(image="neo4j_with_plugins")
         .with_exposed_ports(7687)
         .with_bind_ports(7687, 7687)
       )
     neo4j_container.start()
-    time.sleep(30)
-    # wait_for_logs(neo4j_container, "Started", timeout=30)
-    print("Neo4J Logs:", neo4j_container.get_logs())
+    try:
+      wait_for_logs(neo4j_container, "Started", timeout=30)
+    except Exception as e:
+      print("Neo4J Logs:", neo4j_container.get_logs())
+      raise e
     self.neo4j_container = neo4j_container
+
+    username = "neo4j"
+    password = "neo4j-password"
+    url = f"bolt://{neo4j_container.get_container_host_ip()}:7687"
 
     self.kg = KnowledgeGraph(neo4j_uri=url, neo4j_user=username, neo4j_password=password)
 
