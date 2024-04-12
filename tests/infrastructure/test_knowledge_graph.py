e87e4d6e2194892288023dcaa50faf62b9e9affc
demattosanthony <demattosanthony@gmail.com>
2024-04-12 13:57:49 -0400

    sucessfully using testcontainers for knowledge graph with no warnings

--- tests/infrastructure/test_knowledge_graph.py
+++ tests/infrastructure/test_knowledge_graph.py
@@ -1,5 +1,4 @@
 import unittest
-from testcontainers.neo4j import Neo4jContainer
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 from openoperator.infrastructure.knowledge_graph import KnowledgeGraph
@@ -9,29 +8,23 @@ class TestKnowledgeGraph(unittest.TestCase):
     username = "neo4j"
     password = "neo4j-password"
     url = "bolt://localhost:7687"
-    self.neo4j_container = (
+    neo4j_container = (
         DockerContainer(image="neo4j_with_plugins")
         .with_exposed_ports(7687)
         .with_bind_ports(7687, 7687)
       )
-    self.neo4j_container.start()
+    neo4j_container.start()
+    wait_for_logs(neo4j_container, "Started")
+    self.neo4j_container = neo4j_container
 
-    wait_for_logs(self.neo4j_container, "Started")
-
-    self.knowledge_graph = KnowledgeGraph(neo4j_uri=url, neo4j_user=username, neo4j_password=password)
+    self.kg = KnowledgeGraph(neo4j_uri=url, neo4j_user=username, neo4j_password=password)
 
   def tearDown(self):
     self.neo4j_container.stop()
 
-  # @wait_container_is_ready
-  # def ensure_container_is_ready(self):
-  #     return self.neo4j_container.get_connection_url()
-
   def test_create_session(self):
-    with self.knowledge_graph.neo4j_driver.session() as session:
+    with self.kg.create_session() as session:
       assert session is not None
-      session.close()
-
 
 if __name__ == '__main__':
     unittest.main()
