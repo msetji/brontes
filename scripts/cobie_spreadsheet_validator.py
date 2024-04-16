db4a0f434caef898a8b64653b4be0bc3d78fd396
demattosanthony <demattosanthony@gmail.com>
2024-04-15 20:17:19 -0400

    cobie spreadsheet validator save to output path if provided one. Also updated readme formatting

--- scripts/cobie_spreadsheet_validator.py
+++ scripts/cobie_spreadsheet_validator.py
@@ -4,9 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Validate a COBie spreadsheet')
 parser.add_argument('--path', type=str, help='The path to the COBie spreadsheet')
+parser.add_argument('--output', type=str, help='The path to the output COBie spreadsheet', default=None)
 args = parser.parse_args()
 
 path = args.path
+output_path = args.output
 
 # Validate the COBie spreadsheet
 with open(path, 'rb') as file:
@@ -14,5 +16,8 @@ with open(path, 'rb') as file:
   errors_found, errors, updated_file = spreadsheet.validate()
   if errors_found:
     print(errors)
+    if output_path:
+      with open(output_path, 'wb') as output_file:
+        output_file.write(updated_file)
   else:
     print("No errors found in the COBie spreadsheet!")
