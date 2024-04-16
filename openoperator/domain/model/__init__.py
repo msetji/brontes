336c551f5b308c7e1539705cb6383687c19d7cf2
demattosanthony <demattosanthony@gmail.com>
2024-04-16 11:44:22 -0400

    fixed failed test bc of bad tool import

--- openoperator/domain/model/__init__.py
+++ openoperator/domain/model/__init__.py
@@ -2,7 +2,6 @@ from .portfolio import Portfolio
 from .user import User
 from .facility import Facility
 from .document import Document, DocumentMetadataChunk, DocumentQuery, DocumentMetadata
-from .tool import Tool, ToolParametersSchema
 from .cobie_spreadsheet import COBieSpreadsheet
 from .device import Device, DeviceCreateParams
 from .point import Point, PointReading, PointUpdates, PointCreateParams
