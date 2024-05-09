0e471003beb8ad37b86a845ca414b10197895ac1
Anthony DeMattos <demattosanthony@gmail.com>
2024-05-09 14:52:15 -0400

    fixed formatting in genenv

--- genenv.py
+++ genenv.py
@@ -7,7 +7,7 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
 kv_name = "OpenOperatorKeyVault"
-kv_url = f"https://openoperatorkeyvault.vault.azure.net/"
+kv_url = "https://openoperatorkeyvault.vault.azure.net/"
 
 cred = DefaultAzureCredential()
 
@@ -23,30 +23,30 @@ dev_properties = [prop for prop in secret_properties if prop.name.endswith("DEV"
 
 secrets = {}
 for secret in properties:
-    secret_name = secret.name
-    secret_value = client.get_secret(secret_name).value
-    secrets[secret_name] = client.get_secret(secret_name).value
+  secret_name = secret.name
+  secret_value = client.get_secret(secret_name).value
+  secrets[secret_name] = client.get_secret(secret_name).value
 
 live_secrets = {}
 for secret in live_properties:
-    secret_name = secret.name
-    secret_value = client.get_secret(secret_name).value
-    live_secrets[secret_name] = client.get_secret(secret_name).value
+  secret_name = secret.name
+  secret_value = client.get_secret(secret_name).value
+  live_secrets[secret_name] = client.get_secret(secret_name).value
 
 dev_secrets = {}
 for secret in dev_properties:
-    secret_name = secret.name
-    secret_value = client.get_secret(secret_name).value
-    dev_secrets[secret_name] = client.get_secret(secret_name).value
+  secret_name = secret.name
+  secret_value = client.get_secret(secret_name).value
+  dev_secrets[secret_name] = client.get_secret(secret_name).value
 
 with open(".env.staging", "w") as env_file:
-    for secret_name, secret_value in secrets.items():
-        env_file.write(f'{secret_name.upper().replace("-","_")}="{secret_value}"\n')
+  for secret_name, secret_value in secrets.items():
+    env_file.write(f'{secret_name.upper().replace("-","_")}="{secret_value}"\n')
 
 with open(".env.prod", "w") as env_file:
-    for secret_name, secret_value in live_secrets.items():
-        env_file.write(f'{secret_name.upper().replace("-LIVE","").replace("-","_")}="{secret_value}"\n')
+  for secret_name, secret_value in live_secrets.items():
+    env_file.write(f'{secret_name.upper().replace("-LIVE","").replace("-","_")}="{secret_value}"\n')
 
 with open(".env", "w") as env_file:
-    for secret_name, secret_value in dev_secrets.items():
-        env_file.write(f'{secret_name.upper().replace("-DEV","").replace("-","_")}="{secret_value}"\n')
+  for secret_name, secret_value in dev_secrets.items():
+    env_file.write(f'{secret_name.upper().replace("-DEV","").replace("-","_")}="{secret_value}"\n')
